3074054dc08b6a94e4736ddc6e82a8572d63d940
System Administrator <root@USBUR151004322L.local>
2017-06-01 09:41:57 -0700

    cleanup

--- Block/ProductViewer.php
+++ Block/ProductViewer.php
@@ -3,8 +3,5 @@ namespace JoshSpivey\ProductViewer\Block;
  
 class ProductViewer extends \Magento\Framework\View\Element\Template
 {
-    public function getHelloWorldTxt()
-    {
-        return 'Hello world!';
-    }
+
 }
